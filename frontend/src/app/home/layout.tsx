e1fe914916d1573a09af68ecbf98ec99d5725cff
Alfian Kafilah Baits <alfiank@stellargeloratech.com>
2025-01-05 23:10:04 +0700

    feat(sidebar): add sidebar

---
+++ frontend/src/app/home/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/ui/app-sidebar"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Employee System",
+  description: "Make it Easier!",
+};
+
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  )
+}
---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div className="space-x-5">
+      <h1 className="ml-5 text-lg">Dashboard</h1>
+    </div>
+  );
+}
